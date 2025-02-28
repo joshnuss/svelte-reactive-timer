ff8ac6db792cf31311dd3bdd18cad8dad8605ae1
Josh Nussbaum <joshnuss@gmail.com>
2025-02-28 18:05:33 +0800

    Add Timer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,1 @@
-// Reexport your entry components here
+export { Timer } from '$lib/timer.svelte.js';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Timer } from '$lib/index.js';
+	import { lightFormat } from 'date-fns/lightFormat';
+
+	const timer = new Timer(10_000, { precision: 100 });
+</script>
+
+<h1>Timer</h1>
+<h2>{Math.floor(timer.elapsed / 1000)}s</h2>
+<p>{timer.status}</p>
+<p>remaining: {Math.ceil(timer.remaining / 1000)}s</p>
+<p>{lightFormat(timer.time || new Date(), 'yyyy-MM-dd HH:MM:ss')}</p>
+
+<button onclick={() => timer.start()} disabled={timer.isRunning}>Start</button>
+<button onclick={() => timer.stop()} disabled={timer.isStopped || timer.isPaused}>Stop</button>
+<button onclick={() => timer.pause()} disabled={!timer.isRunning}>Pause</button>
+<button onclick={() => timer.resume()} disabled={!timer.isPaused}>Resume</button>
+<button onclick={() => timer.reset()}>Reset</button>
